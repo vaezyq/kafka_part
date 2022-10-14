932e0d40b12d357b3560c3669d8d1770ae0cab47
MaeEms <751861005@qq.com>
2022-10-14 15:26:16 +0800

    some small changes

--- src/main/java/com/example/kafka_test/utils/ListenerTrainInfoDoorThread.java
+++ src/main/java/com/example/kafka_test/utils/ListenerTrainInfoDoorThread.java
@@ -1,11 +1,13 @@
 package com.example.kafka_test.utils;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class ListenerTrainInfoDoorThread extends Thread {
@@ -24,6 +26,12 @@ public class ListenerTrainInfoDoorThread extends Thread {
         return trainInfoDoor;
     }
 
+    public Map<String, String> removeKeySpace(Map<String, String> map) {
+        Map<String, String> res = new HashMap<>();
+        for (Map.Entry<String, String> entry : map.entrySet())
+            res.put(entry.getKey().trim(), entry.getValue().toString().trim());
+        return res;
+    }
 
     @Override
     public void run() {
@@ -34,15 +42,22 @@ public class ListenerTrainInfoDoorThread extends Thread {
             //空调部分的数据处理
             ConsumerRecords<String, String> records_door = kafkaConsumer_door.poll(500);
 
-
             for (ConsumerRecord<String, String> record : records_door) {
+                Map<String,String> map = removeKeySpace(recordStringProcess.processRecordAndString(record.key().toString(), record.value().toString()));
                 if (trainInfoDoor.containsKey(record.key().toString().substring(0, 4))) {
-                    trainInfoDoor.replace(record.key().toString().substring(0, 4),recordStringProcess.processRecordAndString(record.key().toString(), record.value().toString()));
+                    trainInfoDoor.replace(record.key().toString().substring(0, 4),map);
                 } else {
-                    trainInfoDoor.put(record.key().toString().substring(0, 4), recordStringProcess.processRecordAndString(record.key().toString(), record.value().toString()));
+                    trainInfoDoor.put(record.key().toString().substring(0, 4), map);
                 }
             }
-//            System.out.println(trainInfoDoor);
+//            Iterator<String> iterator = trainInfoDoor.keySet().iterator();
+//            while(iterator.hasNext()){
+//                String trainNum = iterator.next();
+//                Map<String,String> map = trainInfoDoor.get(trainNum);
+//                String str = JSON.toJSONString(map);
+//            }
+            String string = JSON.toJSONString(trainInfoDoor);
+            System.out.println(string);
         }
     }
 }
